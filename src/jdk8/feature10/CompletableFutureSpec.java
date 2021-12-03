47fbf4647e0c9acc19f6aca9e62d9a3cee0acd50
PasseRR <xie__hai@sina.com>
2021-12-03 11:31:17 +0800

    CompletableFuture

---
+++ src/jdk8/feature10/CompletableFutureSpec.java
@@ -0,0 +1,98 @@
+package jdk8.feature10;
+
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * {@link java.util.concurrent.CompletableFuture}
+ * @author xiehai
+ * @date 2021/12/03 10:04
+ */
+public class CompletableFutureSpec {
+    public void run() {
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() ->
+                System.out.printf("%s says, hello world \n", Thread.currentThread().getName())
+            )
+            .thenRunAsync(() -> System.out.printf("%s says, hello world again\n", Thread.currentThread().getName()));
+
+        future.join();
+    }
+
+    public void supply() {
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> "Hello")
+            .thenApplyAsync(s -> {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException ignore) {
+                }
+                System.out.printf("%s says, %s\n", Thread.currentThread().getName(), s);
+                return s + " World";
+            })
+            .thenApplyAsync(s -> {
+                System.out.printf("%s says, %s\n", Thread.currentThread().getName(), s);
+                return s + "!";
+            })
+            .whenComplete((s, e) -> {
+                if (Objects.nonNull(e)) {
+                    System.out.println(e.getMessage());
+                }
+
+                System.out.printf("%s says, %s\n", Thread.currentThread().getName(), s);
+            });
+
+        System.out.println(future.join());
+    }
+
+    public void allOf() {
+        // 异步执行所有任务
+        CompletableFuture.allOf(
+                CompletableFuture.runAsync(() ->
+                    System.out.printf("%s says, hello world \n", Thread.currentThread().getName())
+                ),
+                CompletableFuture.runAsync(() ->
+                    System.out.printf("%s says, hello world again\n", Thread.currentThread().getName())
+                ),
+                CompletableFuture.runAsync(() -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(2);
+                    } catch (InterruptedException ignore) {
+                    }
+                    System.out.printf("%s says, hello world \n", Thread.currentThread().getName());
+                })
+            )
+            // 当所有任务完成时
+            .join();
+    }
+
+    public void combine() {
+        // 异步执行带返回值的任务
+        Integer join = CompletableFuture.completedFuture(1)
+            .thenCombineAsync(CompletableFuture.supplyAsync(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException ignore) {
+                }
+                System.out.printf("calc 2 is thread %s\n", Thread.currentThread().getName());
+                return 2;
+            }), Integer::sum)
+            .thenCombineAsync(CompletableFuture.supplyAsync(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException ignore) {
+                }
+                System.out.printf("calc 3 is thread %s\n", Thread.currentThread().getName());
+                return 3;
+            }), Integer::sum)
+            .join();
+        System.out.println(join);
+    }
+
+    public static void main(String[] args) {
+        CompletableFutureSpec spec = new CompletableFutureSpec();
+//        spec.run();
+        spec.supply();
+//        spec.allOf();
+//        spec.combine();
+    }
+}
