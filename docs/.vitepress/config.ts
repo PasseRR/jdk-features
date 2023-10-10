2ab73ae850a54c8ff529c3521cc6c6b4a8b1ea28
PasseRR <xie__hai@sina.com>
2023-10-10 18:36:32 +0800

    sitemap索引问题

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -50,7 +50,7 @@ export default withMermaid(
             transformItems(items) {
                 return items.map(it => {
                     it.lastmodrealtime = true;
-                    it.url = `/${it.url}`;
+                    it.url = site.base + `/${it.url}`;
 
                     return it;
                 });
